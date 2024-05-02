890fc158f4f282ee93efe248a1cdb0b24e3e318a
Dawa1234 <sdawajangbu99@gmail.com>
2024-05-02 20:52:13 +0545

    added test_cov_console for test coverage / testing completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'dart:async';
 import 'dart:developer';
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pixabay/config/routes/generated_route.dart';
 import 'package:pixabay/config/theme/theme.dart';
+import 'package:pixabay/helper/shared_pref.dart';
 import 'package:pixabay/service/http_execute_api.dart';
 import 'package:pixabay/service/server_config.dart';
 import 'package:pixabay/src/bloc/favorite_cubit/favorite_cubit.dart';
@@ -14,7 +14,7 @@ import 'package:pixabay/src/bloc/search_bloc/search_bloc.dart';
 void main() {
   runZonedGuarded(() async {
     WidgetsFlutterBinding.ensureInitialized();
-    // await SharedPref.init();
+    await SharedPref.init();
     runApp(const AppWrapper());
   }, (error, stack) {
     log(error.toString());
@@ -46,7 +46,7 @@ class _AppWrapperState extends State<AppWrapper> {
         ],
         child: MaterialApp(
             theme: CustomThemeData.themeData(),
-            debugShowCheckedModeBanner: kDebugMode,
+            debugShowCheckedModeBanner: false,
             onGenerateRoute: GeneratedRoute.onGeneratedRoute));
   }
 }
